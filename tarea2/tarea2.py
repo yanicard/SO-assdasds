afbb8ecb943c2860ac0a7809468eb1ba1e3e4bff
Rodrigo Verdugo Meier <rodrigovm@gmail.com>
2011-12-23 21:36:26 -0300

    algunas cosillas

--- tarea2/tarea2.py
+++ tarea2/tarea2.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15
 # Tarea 2 Sistemas Operativos  
-import sys
+import sys 
+import os
 
 if len(sys.argv) == 3:
         if sys.argv[2] < 0:
         	print "La Profundidad no puede ser Negativa"
         else:
-        	print "hola mundo"
+        	# cambia de directorio
+        	# os.chdir(sys.argv[1])
+        	print os.getpid()
+        	# os.pipe()
+        	# direcotrio actual
+        	# print os.getcwd()
+        	for file in os.listdir(sys.argv[1]):
+        		print file
 else:
         print "Este programa necesita un parámetro"
