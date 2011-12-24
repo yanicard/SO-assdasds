f4c4485a8cb1b762639f8b9c9a1efa9a8b8c1636
Rodrigo Verdugo Meier <rodrigovm@gmail.com>
2011-12-23 21:00:24 -0300

    recibe parametros

--- tarea2/tarea2.py
+++ tarea2/tarea2.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python
+# -*- coding: iso-8859-15
 # Tarea 2 Sistemas Operativos  
-print "rusiars"
+import sys
+
+if len(sys.argv) == 3:
+        if sys.argv[2] < 0:
+        	print "La Profundidad no puede ser Negativa"
+        else:
+        	print "hola mundo"
+else:
+        print "Este programa necesita un parámetro"
